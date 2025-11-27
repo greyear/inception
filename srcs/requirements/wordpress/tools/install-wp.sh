1b9f444c2cf3ce192798f763b8da6e34da7ae1b9
Anna Zinchenko <anna.s.zinchenko@gmail.com>
2025-11-27 02:07:12 +0000

    added 2 users. need to: put their data to .env maybe, put all files into tools, work on wordpress pages

---
+++ srcs/requirements/wordpress/tools/install-wp.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+set -e
+
+cd /var/www/html || exit 1
+
+echo "Waiting for MariaDB..."
+until mysqladmin ping -h"$DB_HOST" -u"$DB_USER" -p"$DB_PASS" >/dev/null 2>&1; do
+    sleep 2
+done
+echo "MariaDB is ready!"
+
+# Устанавливаем WP-CLI
+if [ ! -f /usr/local/bin/wp ]; then
+    echo "Installing WP-CLI..."
+    curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+    chmod +x wp-cli.phar
+    mv wp-cli.phar /usr/local/bin/wp
+fi
+
+# Если WP не установлен – ставим
+if [ ! -f wp-config.php ]; then
+
+    echo "Downloading WordPress..."
+    wp core download --allow-root
+
+    echo "Creating wp-config.php..."
+    wp config create \
+        --dbname="$DB_NAME" \
+        --dbuser="$DB_USER" \
+        --dbpass="$DB_PASS" \
+        --dbhost="$DB_HOST" \
+        --allow-root \
+        --skip-check
+
+    echo "Installing WordPress..."
+    wp core install \
+        --url="$WP_URL" \
+        --title="$WP_TITLE" \
+        --admin_user="$WP_ADMIN_USER" \
+        --admin_password="$WP_ADMIN_PASS" \
+        --admin_email="$WP_ADMIN_EMAIL" \
+        --allow-root
+
+    echo "Creating secondary user..."
+    wp user create "$WP_USER" "$WP_USER_EMAIL" \
+        --role=author \
+        --user_pass="$WP_USER_PASS" \
+        --allow-root
+fi
+
+php-fpm82 -F
